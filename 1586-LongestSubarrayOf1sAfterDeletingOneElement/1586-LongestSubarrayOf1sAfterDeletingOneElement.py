11155c77f48f88b0f83d74af267699ddcf17ce3e
DinosaurMauricio <70724167+DinosaurMauricio@users.noreply.github.com>
2025-07-02 14:51:55 +0200

    cleaned one solution from useless code lol

--- 1586-LongestSubarrayOf1sAfterDeletingOneElement/1586-LongestSubarrayOf1sAfterDeletingOneElement.py
+++ 1586-LongestSubarrayOf1sAfterDeletingOneElement/1586-LongestSubarrayOf1sAfterDeletingOneElement.py
@@ -2,23 +2,17 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         left_index = 0
-        # can only remove one so instead just save a boolean
-        removed = False
         max_result = -1
         zero_index = -1
 
         for right_index, val in enumerate(nums):
 
             if val == 0:
-                if not removed:
-                    removed = True
-                else:
-                    left_index = zero_index + 1
+                left_index = zero_index + 1
                 zero_index = right_index
 
-            result = right_index - left_index  # index = 0 so we have to add one
+            result = right_index - left_index
 
             max_result = max(max_result, result)
-
 
         return max_result
