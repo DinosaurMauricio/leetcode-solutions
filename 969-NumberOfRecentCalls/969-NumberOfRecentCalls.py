23d9f9325a41d5bcb5c9332b29a38a58cf093327
DinosaurMauricio <70724167+DinosaurMauricio@users.noreply.github.com>
2025-07-14 10:21:27 +0200

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.py
@@ -0,0 +1,34 @@
+# Last updated: 7/14/2025, 10:21:27 AM
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+
+        i = 0
+        while i < len(self.requests) and self.requests[i] < t - 3000:
+            # don't care about the max range just that its okay with the min range
+            # if it's not in the range we are gonna remove i elements
+            i += 1
+
+        if i != 0:
+            # if different means there where some elements at the start that didn't comply so we remove them
+            self.requests = self.requests[i:]
+
+        return len(self.requests)
+        #counter = 0
+#
+        #self.requests.append(t)
+#
+        #for request in self.requests:
+        #    if t - 3000 <= request <= t:
+        #        counter += 1
+        #return counter
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
